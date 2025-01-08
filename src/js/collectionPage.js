bdfc530664db0b50fac36b0db521ea98ff34f1aa
Your Name <indingenousjungle@gmail.com>
2025-01-08 10:53:26 -0500

    I ended up refactoring the Javascript code for toggling the side bar menu in the collections page. I correctly utilized the forEach method on the arrows array I created, and correctly utilized an if else conditional statement. However instead of creating another forEach iterator function inside the forEach for the arrowArray, I utilized the index parameter inside the callback function of the array array and gave both arrowArray and the locally declared dropDownArray the same index. So that when an arrow is clicked, the corresponding drop down will display.

--- src/js/collectionPage.js
+++ src/js/collectionPage.js
@@ -46,3 +46,69 @@ breakpoints :{
   }
 }
 });
+
+
+//Functionality for side bar toggle display on collections page.
+const collectionMenu = () => {
+
+  const dropDown1 = 
+  document.querySelector('#nftCollection_bottom_box1');
+  const dropDown2 = 
+  document.querySelector('#nftCollection_bottom_box2');
+  const dropDown3 = 
+  document.querySelector('#nftCollection_bottom_box3');
+  const dropDown4 = 
+  document.querySelector('#nftCollection_bottom_box4');
+  const dropDown5 = 
+  document.querySelector('#nftCollection_bottom_box5');
+  const dropDown6 = 
+  document.querySelector('#nftCollection_bottom_box6');
+  const dropDown7 = 
+  document.querySelector('#nftCollection_bottom_box7');
+  const dropDown8 = 
+  document.querySelector('#nftCollection_bottom_box8');
+
+  const dropDownArray = [
+    dropDown1,
+    dropDown2,
+    dropDown3,
+    dropDown4,
+    dropDown5,
+    dropDown6,
+    dropDown7,
+    dropDown8
+  ];
+
+  const arrow1 = 
+  document.querySelector('#nftCollection_dropDown_arrow1');
+  const arrow2 = 
+  document.querySelector('#nftCollection_dropDown_arrow2');
+  const arrow3 = 
+  document.querySelector('#nftCollection_dropDown_arrow3');
+  const arrow4 = 
+  document.querySelector('#nftCollection_dropDown_arrow4');
+  const arrow5 = 
+  document.querySelector('#nftCollection_dropDown_arrow5');
+  const arrow6 = 
+  document.querySelector('#nftCollection_dropDown_arrow6');
+  const arrow7 = 
+  document.querySelector('#nftCollection_dropDown_arrow7');
+  const arrow8 = 
+  document.querySelector('#nftCollection_dropDown_arrow8');
+
+  const arrowArray = [arrow1, arrow2, arrow3, arrow4, arrow5, arrow6, arrow7, arrow8];
+
+  arrowArray.forEach((arrow, index) => {
+    arrow.addEventListener('click', () => {
+      const dropDown = dropDownArray[index];
+      if(dropDown.style.display === 'none' || dropDown.style.display === ''){
+        dropDown.style.display = 'block';
+      } else {
+        dropDown.style.display = 'none';
+      }
+    })
+  });
+
+};
+
+collectionMenu();
